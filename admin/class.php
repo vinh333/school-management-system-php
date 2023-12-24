c9b41143edc7c6e1dc38e78c866949cae5cf2970
Vinh3 <101193855+vinh333@users.noreply.github.com>
2023-12-24 16:56:44 +0700

    1

--- admin/class.php
+++ admin/class.php
@@ -85,7 +85,7 @@ if (isset($_SESSION['admin_id']) &&
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>	
     <script>
         $(document).ready(function(){
-             $("#navLinks li:nth-child(6) a").addClass('active');
+             $("#navLinks li:nth-child(4) a").addClass('active');
         });
     </script>
 
--- admin/settings.php
+++ admin/settings.php
@@ -81,7 +81,7 @@ if (isset($_SESSION['admin_id']) &&
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>	
     <script>
         $(document).ready(function(){
-             $("#navLinks li:nth-child(10) a").addClass('active');
+             $("#navLinks li:nth-child(8) a").addClass('active');
         });
     </script>
 
