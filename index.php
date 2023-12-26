cbc595f234a9fbbdfcf6d6e2f2b343e35ea71e4b
Vinh3 <101193855+vinh333@users.noreply.github.com>
2023-12-26 17:31:57 +0700

    1

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ if ($setting != 0) {
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="icon" href="logo.png">
+		
 	</head>
 
 	<body class="body-home">
